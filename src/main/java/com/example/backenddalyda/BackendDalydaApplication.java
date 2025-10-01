f2ef6e2ba13bf52708acee9e0f1a0cebbfa323bd
davidhitman <hitimanadav47@gmail.com>
2025-10-01 21:29:18 +0300

    Initial Commit

---
+++ src/main/java/com/example/backenddalyda/BackendDalydaApplication.java
@@ -0,0 +1,13 @@
+package com.example.backenddalyda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDalydaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendDalydaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backenddalyda/BackendDalydaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backenddalyda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDalydaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
